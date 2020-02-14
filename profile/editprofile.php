52b32c385edda0748e3daea4202517cfbe39f1ab
Monster <39898579+Monster2299@users.noreply.github.com>
2020-02-15 01:28:10 +0530

    Add files via upload

---
+++ profile/editprofile.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>talkie</title>
+    <link rel="stylesheet" href="editp.css">
+</head>
+<body>
+   <div class="add">
+   <form action="" method="post">
+   <label for="work details">working details</label>
+   <input type="text" placeholder="currently studing or working " name="wrk1">
+   <input type="submit" value="publish" name="submit">
+   </form>
+   </div> 
+</body>
+</html>
